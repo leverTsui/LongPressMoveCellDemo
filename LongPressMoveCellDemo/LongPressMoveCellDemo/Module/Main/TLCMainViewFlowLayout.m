50f5ada6e5419c93d1f2ca732439886e9f448b86
316495 <18682316495@163.com>
2018-01-31 19:59:43 +0800

    FIX:iOS8 和 iOS9 "以后要做"不会回弹问题修复

--- LongPressMoveCellDemo/LongPressMoveCellDemo/Module/Main/TLCMainViewFlowLayout.m
+++ LongPressMoveCellDemo/LongPressMoveCellDemo/Module/Main/TLCMainViewFlowLayout.m
@@ -21,34 +21,37 @@
     [super prepareLayout];
 }
 
-- (CGFloat)pageWidth {
+- (CGFloat)tlc_pageWidth {
     return self.itemSize.width + self.minimumLineSpacing;
 }
 
 - (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity {
-    CGFloat rawPageValue = self.collectionView.contentOffset.x / self.pageWidth;
+    CGFloat rawPageValue = self.collectionView.contentOffset.x / [self tlc_pageWidth];
     CGFloat currentPage = (velocity.x > 0.0) ? floor(rawPageValue) : ceil(rawPageValue);
     CGFloat nextPage = (velocity.x > 0.0) ? ceil(rawPageValue) : floor(rawPageValue);
     
     BOOL pannedLessThanAPage = fabs(1 + currentPage - rawPageValue) > 0.5;
-    BOOL flicked = fabs(velocity.x) > [self flickVelocity];
+    BOOL flicked = fabs(velocity.x) > [self tlc_flickVelocity];
     CGFloat actualPage = 0.0;
     
     if (pannedLessThanAPage && flicked) {
-        proposedContentOffset.x = nextPage * self.pageWidth;
+        proposedContentOffset.x = nextPage * [self tlc_pageWidth];
         actualPage = nextPage;
     } else {
-        proposedContentOffset.x = round(rawPageValue) * self.pageWidth;
+        proposedContentOffset.x = round(rawPageValue) * [self tlc_pageWidth];
         actualPage = round(rawPageValue);
-    }
+    } 
     if (lround(actualPage) >= 1) {
         proposedContentOffset.x -= 4;
     } 
+    if (lround(actualPage) >= 2) {
+        proposedContentOffset.x = self.collectionView.contentSize.width - TLCScreenWidth;
+    }
     
     return proposedContentOffset;
 }
 
-- (CGFloat)flickVelocity {
+- (CGFloat)tlc_flickVelocity {
     return 0.3;
 }
 
